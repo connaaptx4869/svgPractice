f66640e647f40cc27891c1a9704e4b2fea473e19
王真 <wangzhen@hz-health.cn>
2018-04-03 09:39:34 +0800

    update

---
+++ app/src/main/java/com/parker/svgpractice/MainActivity.java
@@ -0,0 +1,17 @@
+package com.parker.svgpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
