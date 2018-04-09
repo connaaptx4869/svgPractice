f854a8a564c24367842d2eed6b946b0054f5509b
APTX4869\zhenh <zhen.hy@qq.com>
2018-04-10 00:44:18 +0800

    UPDATE

--- app/src/main/java/com/parker/svgpractice/SvgAnimStarActivity.java
+++ app/src/main/java/com/parker/svgpractice/SvgAnimStarActivity.java
@@ -6,16 +6,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
 
-import com.parker.svgpractice.databinding.ActivitySvgAnimBinding;
+import com.parker.svgpractice.databinding.ActivitySvgAnimStarBinding;
 
 /**
  * Author : zhenh.
  * Created by Orz on 2018/4/3 12:31.
  */
 
-public class SvgAnimActivity extends AppCompatActivity {
-    private ActivitySvgAnimBinding mBinding;
+public class SvgAnimStarActivity extends AppCompatActivity {
+    private ActivitySvgAnimStarBinding mBinding;
 
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
@@ -24,12 +25,15 @@ public class SvgAnimActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_svg_anim);
-        mBinding.anim.setPivotX(100);
-        mBinding.anim.setPivotY(100);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_svg_anim_star);
 //        AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(
 //                this, R.drawable.ic_rabbit_anim);
 //        mBinding.anim.setImageDrawable(animatedVectorDrawableCompat);
-        ((Animatable) mBinding.anim.getDrawable()).start();
+        mBinding.anim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Animatable) mBinding.anim.getDrawable()).start();
+            }
+        });
     }
 }
