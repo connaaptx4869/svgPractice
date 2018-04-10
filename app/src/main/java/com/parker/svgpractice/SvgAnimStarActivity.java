ad95eaf82fcb8d1c8f5cdd1dbe5a81d5e6a1b6ff
王真 <wangzhen@hz-health.cn>
2018-04-10 11:54:13 +0800

    星星动画

--- app/src/main/java/com/parker/svgpractice/SvgAnimStarActivity.java
+++ app/src/main/java/com/parker/svgpractice/SvgAnimStarActivity.java
@@ -3,6 +3,8 @@ package com.parker.svgpractice;
 import android.databinding.DataBindingUtil;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
@@ -17,22 +19,40 @@ import com.parker.svgpractice.databinding.ActivitySvgAnimStarBinding;
 
 public class SvgAnimStarActivity extends AppCompatActivity {
     private ActivitySvgAnimStarBinding mBinding;
+    private boolean checked;
+    private boolean isInAnim;
 
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
 
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (checked) {
+                mBinding.anim.setImageResource(R.drawable.ic_collection_anim_check);
+            } else {
+                mBinding.anim.setImageResource(R.drawable.ic_collection_anim_uncheck);
+            }
+            isInAnim = false;
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_svg_anim_star);
-//        AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(
-//                this, R.drawable.ic_rabbit_anim);
-//        mBinding.anim.setImageDrawable(animatedVectorDrawableCompat);
         mBinding.anim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (isInAnim) {
+                    return;
+                }
+                isInAnim = true;
                 ((Animatable) mBinding.anim.getDrawable()).start();
+                checked = !checked;
+                Message message = handler.obtainMessage();
+                handler.sendMessageDelayed(message, 300);
             }
         });
     }
